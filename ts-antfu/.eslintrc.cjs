a804a463613fa7828276631557bad069d991233c
Dmytro <dmytro.cisar@gmail.com>
2023-06-29 20:37:22 +0300

    feat: update ts-antfu

--- ts-antfu/.eslintrc.cjs
+++ ts-antfu/.eslintrc.cjs
@@ -1,3 +1,18 @@
 module.exports = {
   extends: '@antfu',
+  plugins: ['no-autofix'],
+  rules: {
+    'space-before-function-paren': 0,
+    '@typescript-eslint/space-before-function-paren': [
+      2,
+      {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always',
+      },
+    ],
+
+    'unused-imports/no-unused-imports': 0, // look below
+    'no-autofix/unused-imports/no-unused-imports': 2,
+  },
 }
