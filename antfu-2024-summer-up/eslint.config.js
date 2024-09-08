d6f7288556c228d6c2a1bc79d078f3f55ebb013d
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-09-08 12:21:11 +0200

    build: antfu-2024-summer-up works

---
+++ antfu-2024-summer-up/eslint.config.js
@@ -0,0 +1,24 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    unocss: true,
+    vue: true,
+  },
+  {
+    ignores: ['demo/input.ts'],
+  },
+  {
+    rules: {
+      'style/space-before-function-paren': [
+        2,
+        {
+          anonymous: 'always',
+          named: 'always',
+          asyncArrow: 'always',
+        },
+      ],
+      'ts/prefer-function-type': 2,
+    },
+  },
+)
