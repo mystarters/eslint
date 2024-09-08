eaa0ee1c3ebb17b3731c5b5bf02740964b178892
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-09-08 13:35:30 +0200

    update files

--- antfu-2024-autumn-test/demo/input.ts
+++
@@ -1,9 +0,0 @@
-interface IF {
-  (): number;
-}
-
-export function fn1() {
-  return 11;
-}
-
-export const fn2: IF = () => 11
--- antfu-2024-autumn-test/demo/output.ts
+++
@@ -1,7 +0,0 @@
-type IF = () => number
-
-export function fn1 () {
-  return 11
-}
-
-export const fn2: IF = () => 11
