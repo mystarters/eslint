373ab35ef9d6b6a4f60c2003a76607dcaff12b4e
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-10-05 14:57:49 +0200

    simpleImportSort works with vue

---
+++ antfu-2024-simple-imports/src/1.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { SOMETHING } from '../something'
+
+const v = ref(SOMETHING)
+</script>
+
+<template>
+  <div>
+    {{ v }}
+  </div>
+</template>
